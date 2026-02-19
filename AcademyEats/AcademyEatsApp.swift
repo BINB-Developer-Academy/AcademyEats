69118659dd18c877890a6a690b2312d99ef97e9e
Mila <alfitriv@gmail.com>
2026-02-19 14:13:48 +0800

    Initial Commit

---
+++ AcademyEats/AcademyEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyEatsApp.swift
+//  AcademyEats
+//
+//  Created by Vania Radmila Alfitri on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AcademyEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
